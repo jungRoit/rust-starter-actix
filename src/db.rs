61906f83668f50a267fb7018822a99a2da2c3d60
silwalanish <silwalanish@gmail.com>
2020-11-24 21:06:16 +0545

    Run code formatter.

--- src/db.rs
+++ src/db.rs
@@ -2,10 +2,10 @@ use mongodb::{options::ClientOptions, Client};
 use std::env;
 
 pub fn create_connection() -> mongodb::Database {
-  let database_url = env::var("DB_URL").expect("DATABASE_URL Environment Variable is not set");
-  let client_options = ClientOptions::parse(&database_url).unwrap();
-  let client = Client::with_options(client_options).unwrap();
+    let database_url = env::var("DB_URL").expect("DATABASE_URL Environment Variable is not set");
+    let client_options = ClientOptions::parse(&database_url).unwrap();
+    let client = Client::with_options(client_options).unwrap();
 
-  let database_name = env::var("DB_NAME").expect("DB_NAME Environment Variable is not set");
-  return client.database(&database_name);
+    let database_name = env::var("DB_NAME").expect("DB_NAME Environment Variable is not set");
+    return client.database(&database_name);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
-use dotenv::dotenv;
 use actix_web::{App, HttpServer};
+use dotenv::dotenv;
 use std::env;
+
 use service::user_service::UserService;
 use service_manager::ServiceManager;
 
-mod db;
 mod controllers;
-mod service;
 mod dao;
+mod db;
 mod entity;
+mod service;
 mod service_manager;
 
 struct AppState {
-    service_manager: ServiceManager
+    service_manager: ServiceManager,
 }
 
 #[actix_rt::main]
-async fn main()-> std::io::Result<()> {
+async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     let server_url = env::var("HOST").expect("SERVER_URL Environment variable is not set.");
     let database = db::create_connection();
 
     // let user_collection = database.collection("User");
-    
+
     HttpServer::new(move || {
         let user_service_worker = UserService::new(database.clone());
         let service_manager = ServiceManager::new(user_service_worker);
         App::new()
-        .data(AppState {service_manager})
-        .configure(controllers::user_controller::init)
-
+            .data(AppState { service_manager })
+            .configure(controllers::user_controller::init)
     })
     .bind(server_url)?
     .run()
